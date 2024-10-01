eb5b76d356e9ab56b145132276723919d5d01ef2
JUHEEBAEK <bjh9287@naver.com>
2024-10-02 02:00:27 +0900

    Fix: Image 따로 컴포넌트로 뺴기 - image가 느리게 로드되서 로딩될 때 스핀 애니메이션 주기 - 순서 변경 - 주석제거, 코드 정리, 변수명 맞추기

--- apps/frontend/src/components/dashboard/ModalDetails.tsx
+++ apps/frontend/src/components/dashboard/ModalDetails.tsx
@@ -1,17 +1,17 @@
 import Button from '@/components/common/Button';
 import Loading from '@/components/common/Loading';
+import ImageBox from '@/components/common/ImageBox';
 import { addCommasToNumber } from '@/utils/number';
 
 import * as S from './ModalDetails.styles';
 
 interface Props {
   isLoading: boolean;
-  info: ICustomerPurchaseDetails[];
+  items: ICustomerPurchaseDetails[];
   onClose: () => void;
 }
 
-export default function ModalDetails({ isLoading, info, onClose }: Props) {
-  console.log(info);
+export default function ModalDetails({ isLoading, items, onClose }: Props) {
   return (
     <S.ModalWrapper>
       <S.Overlay onClick={onClose}></S.Overlay>
@@ -24,25 +24,28 @@ export default function ModalDetails({ isLoading, info, onClose }: Props) {
             </Button>
           </S.Header>
           <S.Content>
-            {isLoading && <Loading />}
-            <S.ListWrapper>
-              {info.length !== 0
-                ? info.map((item, index) => {
-                    const { date, imgSrc, price, product, quantity } = item;
-                    return (
-                      <S.ListItem key={index}>
-                        <span>{date}</span>
-                        <span>
-                          <img src={imgSrc} alt={`${product}-image`} width={100} height={100} />
-                        </span>
-                        <span>{quantity}</span>
-                        <span>{product}</span>
-                        <span>{addCommasToNumber(price)}</span>
-                      </S.ListItem>
-                    );
-                  })
-                : '구매한 내역이 없습니다.'}
-            </S.ListWrapper>
+            {isLoading ? (
+              <Loading />
+            ) : (
+              <S.ListWrapper>
+                {items.length !== 0
+                  ? items.map((item, index) => {
+                      const { date, imgSrc, price, product, quantity } = item;
+                      return (
+                        <S.ListItem key={index}>
+                          <span>{date}</span>
+                          <span>
+                            <ImageBox path={imgSrc} alt={`${product}-image`} width={80} height={80} />
+                          </span>
+                          <span>{product}</span>
+                          <span>{quantity}</span>
+                          <span>{addCommasToNumber(price)}</span>
+                        </S.ListItem>
+                      );
+                    })
+                  : '구매한 내역이 없습니다.'}
+              </S.ListWrapper>
+            )}
           </S.Content>
         </S.Container>
       </S.ModalScrollPaper>
