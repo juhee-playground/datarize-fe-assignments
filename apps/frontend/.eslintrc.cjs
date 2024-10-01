7339e67fe3000054678a07171b5849bf39c6ff42
JUHEEBAEK <bjh9287@naver.com>
2024-10-01 17:49:00 +0900

    Chore: 라이브러리 설치 및 lint 세팅

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -4,8 +4,81 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+    'consistent-return': 'error',
+    eqeqeq: ['error', 'always'],
+    'prefer-const': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': 'warn',
+    'no-implicit-globals': 'error',
+    'no-shadow': 'error',
+    'no-magic-numbers': ['warn', { ignore: [0, 1], ignoreArrayIndexes: true }],
+    'no-var': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', // node.js내의 모듈
+          'external', // 외부 패키지 및 라이브러리
+          'internal', // 프로젝트 내부의 모듈
+          ['parent', 'sibling'], // 부모(상위) 디렉토리의 모듈, 동일한 디렉토리의 모듈
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**/*',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: './**/*.styles',
+            group: 'sibling',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always-and-inside-groups',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^T[A-Z]',
+          match: true,
+        },
+      },
+    ],
   },
 }
